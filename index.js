83d5dc6b3013949c9c5a2aa2016abdffb99d675d
UglyValfi <ValfiBrind@gmail.com>
2022-02-10 14:16:10 +0200

    tasks

---
+++ index.js
@@ -0,0 +1,121 @@
+// TASK 1
+
+// str ='abcde'
+// console.log(str[0])
+// console.log(str[1])
+// console.log(str[4])
+
+// TASK 2
+
+
+// alert(60*60)
+// alert(60*60*24)
+
+//TASK 3
+
+// let number = 1
+// number = number +12
+// number = number -14
+// number = number *5
+// number = number /7 
+// number = number +1
+// number = number -1
+// console.log(number)
+
+//TASK 4 
+
+// let num = 3
+// alert (num)
+
+//TASK 5 
+
+// let a = 10
+// let b = 2
+// console.log(a+b)
+// console.log(a-b)
+// console.log(a*b)
+// console.log(a/b)
+
+//TASK 6
+
+// let c = 15
+// let d= 2
+// let result = c+d
+// console.log(result)
+
+//TASK 7
+
+// let a=10
+// let b=2
+// let c=5
+// console.log(a+b+c)
+
+//TASK 8 
+
+// let a=17
+// let b=10
+// let c = a-b
+// let d = 7
+// let result = c+d
+// console.log(result)
+
+//TASK 9 
+
+// let str = "Hello World"
+// alert(str)
+
+//TASK 10
+
+// let str1="Hello"
+// let str2=" World"
+// alert(str1+str2)
+
+//TASK 11
+
+// let myName = " Даня"
+// let str = "Привет"
+// alert(str +  myName)
+
+//TASK 12
+
+// let age = 14
+// let str = "Мне"
+// let str2 = " Лет"
+// alert(str + age+ str2)
+
+//TASK 13
+
+// let name = prompt('Ваше имя', "Даня")
+// alert(`Ваше имя ${name}`)
+
+//TASK 14
+
+// let numb = prompt ('Какой квадрат числа 4?', 2)
+// alert (`${numb} в квадрате`)
+
+//TASK 15 
+
+// let str = 'abcde'
+// console.log(str[0])
+// console.log(str[2])
+// console.log(str[4])
+
+//TASK 16
+
+// let num = 12345
+
+
+//TASK 17
+// alert(60*60) //Секунд в минуте.
+// alert(60*1140) //Секунд в неделе.
+// alert(60*43800) //Секунд в месяце. 
+
+//TASK 18
+
+// let hour = "14:"
+// let min = "08:"
+// let sec = "07"
+// alert(hour+min+sec)
+
+//TASK 19
+???
